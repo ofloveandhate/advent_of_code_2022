ad68fdc0f1aa9d7ad4c3f0aa2009c18cb9c4c80c
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2022-12-13 11:55:05 -0600

    improved template solution and prep code

--- solution.py
+++ solution.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
 
+test = False
+
+
+if test:
+	input_name = 'testinput.txt'
+
+else:
+	input_name = 'input.txt'
+
+	
 def read_data():
-	with open ('testinput.txt') as f:
+	with open (input_name) as f:
 		data = f.readlines()
 	return [d.strip() for d in data]
 
